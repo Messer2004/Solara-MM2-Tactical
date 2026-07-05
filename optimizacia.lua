ddda282bf7f21a22a65ebe1bde07d19159b6ac26
Messer2004 <razgrom735@gmail.com>
2026-07-05 23:21:07 +0300

    Update optimizacia.lua

--- optimizacia.lua
+++ optimizacia.lua
@@ -30,14 +30,17 @@ if cam then
 end
 
 local function optimize(v)
-    if v:IsA("BasePart") then
+    if v:IsA("BasePart") and not v:IsA("Terrain") then
         local name = v.Name:lower()
-        if not v.CanCollide and (name:find("grass") or name:find("bush") or name:find("plant") or name:find("foliage") or v.Material == Enum.Material.Leaves) then
+        if name:find("grass") or name:find("bush") or name:find("plant") or name:find("foliage") or name:find("tree") or name:find("leaf") or name:find("leaves") or name:find("trunk") then
             v:Destroy()
             return
         end
         v.Material = Enum.Material.SmoothPlastic
         v.CastShadow = false
+    end
+    if v:IsA("MeshPart") then
+        v.TextureID = ""
     end
     if v:IsA("Texture") or v:IsA("Decal") or v:IsA("ParticleEmitter") or v:IsA("Smoke") or v:IsA("Fire") or v:IsA("Sparkles") then
         v:Destroy()
